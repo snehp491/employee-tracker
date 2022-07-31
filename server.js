f7e6cca5825377fe9216e2f10fbf3c7d828a8a85
Andrew Blejde <snehp491@gmail.com>
2022-07-31 19:49:10 -0400

    add seed data and first pass of methods

--- server.js
+++ server.js
@@ -15,15 +15,19 @@ connection.connect(function (err) {
     }
     console.log("connected as id " + connection.threadId);
     console.log('Welcome to the Employee Database');
-    // view all departments,
-    // view all roles,
-    // view all employees,
-    // add a department,
-    // add a role,
-    // add an employee,
-    // update an employee role
+    start();
+});
+
+function start() {
     inquirer.prompt({
         /* Pass your questions in here */
+        // view all departments,
+        // view all roles,
+        // view all employees,
+        // add a department,
+        // add a role,
+        // add an employee,
+        // update an employee role
         type: 'list',
         name: 'answer',
         choices: [
@@ -35,9 +39,26 @@ connection.connect(function (err) {
             'Add an Employee',
             'Update an Employee Role'
         ]
-    }).then((answer) => {
+    }).then((response) => {
             // Use user feedback for... whatever!!
+        const answer = response['answer'];
         console.log(answer);
+        console.log(answer === 'View All Departments');
+        if (answer === 'View All Departments') {
+            getAllDepartments(answer);
+        } else if (answer === 'View All Roles') {
+            getAllRoles(answer);
+        } else if (answer === 'View All Employees') {
+            getAllEmployees(answer);
+        } else if (answer === 'Add a Department') {
+            addDepartment(answer);
+        } else if (answer === 'Add a Role') {
+            addRole(answer);
+        } else if (answer === 'Add an Employee') {
+            addEmployee(answer);
+        } else if (answer === 'Update an Employee Role') {
+            updateEmployeeRole(answer);
+        }
         })
         .catch((error) => {
             if (error.isTtyError) {
@@ -46,4 +67,200 @@ connection.connect(function (err) {
                 // Something else went wrong
             }
         });
-});
+}
+
+function getAllDepartments(question) {
+    const query =
+        `SELECT D.ID as 'Department ID', D.NAME as 'Department Name'
+        from DEPARTMENT D`;
+
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        console.log('Completed ' + question);
+
+        start();
+    });
+}
+
+function getAllRoles(question) {
+    const query =
+        `SELECT R.TITLE as 'Job Title', R.ID as 'Role ID', D.NAME as 'Department', R.SALARY as 'Salary'
+         from ROLE R
+                  join DEPARTMENT D on R.ID = D.ID`;
+
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        console.log('Completed ' + question);
+
+        start();
+    });
+}
+
+function getAllEmployees(question) {
+
+    const query =
+        `SELECT E.ID as 'Employee ID', 
+            E.FIRST_NAME as 'First Name', 
+            E.LAST_NAME as 'Last Name', 
+            R.TITLE as 'Job Title', 
+            D.NAME as 'Department', 
+            R.SALARY as 'Salary' from EMPLOYEE E
+            JOIN ROLE R on E.ROLE_ID = R.ID 
+            JOIN DEPARTMENT D on R.DEPARTMENT_ID = D.ID`;
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        console.log('Completed ' + question);
+
+        start();
+    });
+}
+
+function addDepartment(question) {
+    inquirer
+        .prompt(
+            {
+                type: "input",
+                name: "department_name",
+                message: "Enter the department name"
+            }).then(
+        (response) => {
+            const department_name = response['department_name'];
+            const query =
+                `INSERT INTO DEPARTMENT SET ?`;
+
+            connection.query(query, {NAME: department_name},
+                function (err, res) {
+                if (err) throw err;
+
+                console.table(res);
+                console.log('Completed ' + question);
+
+                start();
+            });
+        }
+    );
+}
+
+function addRole(question) {
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "role_name",
+                message: "Enter the role name"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Enter the salary"
+            },
+            {
+                type: "input",
+                name: "department_id",
+                message: "Enter the department id",
+            }]).then(
+        (response) => {
+            const role_name = response['role_name'];
+            const salary = response['salary'];
+            const department_id = response['department_id'];
+
+            const query =
+                `INSERT INTO ROLE SET ?`;
+
+            connection.query(query, {TITLE: role_name, SALARY: salary, DEPARTMENT_ID: department_id},
+                function (err, res) {
+                    if (err) throw err;
+
+                    console.table(res);
+                    console.log('Completed ' + question);
+
+                    start();
+                });
+        }
+    );
+}
+
+function addEmployee(question) {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "Enter the employee first name"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "Enter the employee last name"
+            },
+            {
+                type: "input",
+                name: "role_id",
+                message: "Enter the role id",
+            },
+            {
+                type: "input",
+                name: "manager_id",
+                message: "Enter the manager id",
+            }]).then(
+        (response) => {
+            const first_name = response['first_name'];
+            const last_name = response['last_name'];
+            const role_id = response['role_id'];
+            const manager_id = response['manager_id'];
+
+            const query =
+                `INSERT INTO EMPLOYEE SET ?`;
+
+            connection.query(query, {FIRST_NAME: first_name, LAST_NAME: last_name, ROLE_ID: role_id, MANAGER_ID: manager_id},
+                function (err, res) {
+                    if (err) throw err;
+
+                    console.table(res);
+                    console.log('Completed ' + question);
+
+                    start();
+                });
+        }
+    );
+}
+
+function updateEmployeeRole(question) {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "employee_id",
+                message: "Enter the employee ID to update the role for"
+            },
+            {
+                type: "input",
+                name: "role_id",
+                message: "Enter the employees new role"
+            }]).then(
+        (response) => {
+            const employee_id = response['employee_id'];
+            const role_id = response['role_id'];
+
+            const query =
+                `UPDATE employee SET role_id = ? WHERE id = ?`;
+
+            connection.query(query, [role_id, employee_id],
+                function (err, res) {
+                    if (err) throw err;
+
+                    console.table(res);
+                    console.log('Completed ' + question);
+
+                    start();
+                });
+        }
+    );
+}
